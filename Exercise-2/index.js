1758608b9c6297f0bb6af89aceb25b42e0bbb5e8
cristian pereiro <139070698+cristianpglz@users.noreply.github.com>
2025-08-30 20:09:37 +0200

    add Exercise-2

--- Exercise-2/index.js
+++ Exercise-2/index.js
@@ -9,3 +9,36 @@ console.log(list);
 const toy = {name: 'Bus laiyiar', date: '20-30-1995', color: 'multicolor'};
 const copyToy = {...toy};
 console.log(copyToy);
+
+
+
+
+
+
+
+const pointsLis2 = [54,87,99,65,32];
+const unitedLists = [...pointsList, ...pointsLis2];
+console.log(unitedLists);
+
+
+
+
+
+const toyUpdate = {lights: 'rgb', power: ['Volar like a dragon', 'MoonWalk']}
+const unitedToys = {...toy, ...toyUpdate};
+console.log(unitedToys);
+
+
+
+
+
+
+
+
+const colors = ['rojo', 'azul', 'amarillo', 'verde', 'naranja'];
+const duplicateColors = [...colors];
+duplicateColors.splice(2,1);
+console.log(duplicateColors);
+
+
+
